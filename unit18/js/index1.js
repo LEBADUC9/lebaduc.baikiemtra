19d2d7a7b2d3303cfd073c593f902c7d8371caee
LEBADUC9 <duc9999999991@gmail.com>
2022-04-20 10:11:22 +0700

    update bai kiem tra

--- unit18/js/index1.js
+++ unit18/js/index1.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
 	while (! isNaturalNumber (n)) {
 		n = prompt ('enter NaturalNumber');
 	}
-	console.log(n.val());
-
-	$('#hi').css({
-		'background-color':'red',
-	})
+	for(var j=1; j<=n; j++){
+		for(var i=1; i<=n; i++){
+			if(i==1 || i==n || j==1 || j==n){
+				document.write('*');
+			}
+			else{
+				document.write('_');
+			}
+		}
+		document.write('<br>')
+	}
 
 	function isNaturalNumber(n){
-    n = n.toString(); 
-    var n1 = Math.abs(n),
-    	n2 = parseInt(n, 10);
-    return !isNaN(n1) && n2 === n1 && n1.toString() === n; 
-}
+    	n = n.toString(); 
+    	var n1 = Math.abs(n),
+    		n2 = parseInt(n, 10);
+    	return !isNaN(n1) && n2 === n1 && n1.toString() === n; 
+	}
 });
