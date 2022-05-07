81ed7dea4eb3e8afcd4b8c3e78966a1cf85673cf
LEBADUC9 <duc9999999991@gmail.com>
2022-05-08 00:54:22 +0700

    first project

---
+++ first project/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.fa-bars').on('click', function(){
+        $('.menu-mb').toggle();
+        $('.fa-bars').toggle();
+        $('.fa-times').toggle();
+    })
+     $('.fa-times').on('click', function(){
+        $('.menu-mb').toggle();
+        $('.fa-bars').toggle();
+        $('.fa-times').toggle();
+    })
+});
